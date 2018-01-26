a11f41264b8490f99c96b5d2ed14667eecd53293
Scott Alexander-Bown <github@alexander-bown.com>
2018-01-26 12:57:55 +0000

    Added Login and login secure mode buttons

--- sample-kotlin/src/main/kotlin/net/helpscout/samples/beacon/kotlin/MainActivity.kt
+++ sample-kotlin/src/main/kotlin/net/helpscout/samples/beacon/kotlin/MainActivity.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
+import com.helpscout.beacon.Beacon
 import com.helpscout.beacon.BeaconActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val userEmail = "jamie+aaaab@mcdg.com"
+    private val signature = "9222339b1df2e54be107d3d5dd6a5124dbcc7310354ff8f003d32cd7373a66e1"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +23,20 @@ class MainActivity : AppCompatActivity() {
 
         findViewById<Button>(R.id.button_open_beacon).setOnClickListener {
             BeaconActivity.open(baseContext)
+        }
+
+        findViewById<Button>(R.id.button_login_beacon).setOnClickListener {
+            Beacon.login(userEmail)
+        }
+
+        findViewById<Button>(R.id.button_secure_login_beacon).setOnClickListener {
+            Beacon.loginSecureMode(userEmail, signature)
+        }
+
+        findViewById<Button>(R.id.button_logout_beacon).setOnClickListener {
+            if (Beacon.getInstance().isLoggedIn) {
+                Beacon.logout()
+            }
         }
 
     }
--- sample-kotlin/src/main/kotlin/net/helpscout/samples/beacon/kotlin/MainApplication.kt
+++ sample-kotlin/src/main/kotlin/net/helpscout/samples/beacon/kotlin/MainApplication.kt
@@ -5,6 +5,10 @@ import com.helpscout.beacon.Beacon
 
 class MainApplication : Application() {
 
+    //from jamie (secure mode only)
+    val beaconId = "d6fdc98c-ac87-4970-b580-7d2ebd38311d"
+    //val beaconId = "a63de347-8a4a-4fe7-9d73-6ae41421d140"
+
     override fun onCreate() {
         super.onCreate()
         initBeacon()
@@ -13,7 +17,7 @@ class MainApplication : Application() {
     fun initBeacon() {
         Beacon.Builder()
                 .withContext(this)
-                .withBeaconId("a63de347-8a4a-4fe7-9d73-6ae41421d140")
+                .withBeaconId(beaconId)
                 .withLogsEnabled(true)
                 .build()
     }
