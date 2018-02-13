14363ca427ec80ef3d03f074b6d30d22b5df1a2f
David González <malmstein@gmail.com>
2018-02-13 15:53:11 +0100

    adding support library and coroutines and dependencies

--- sample-core/src/main/java/com/helpscout/beacon/sample/core/CoreActivity.kt
+++ sample-core/src/main/java/com/helpscout/beacon/sample/core/CoreActivity.kt
@@ -1,12 +1,17 @@
 package com.helpscout.beacon.sample.core
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.helpscout.beacon.Beacon
+import net.helpscout.samples.beacon.core.R
 
 class CoreActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_core)
+
+        val repository = Beacon.getInstance().repositoryInstance
+
     }
 }
