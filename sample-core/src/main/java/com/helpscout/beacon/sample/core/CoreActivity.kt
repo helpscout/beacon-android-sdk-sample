0acbee8164404ff0e63f9ebeec25193e4cd94263
David González <malmstein@gmail.com>
2018-02-13 15:36:28 +0100

    adding core sample module

---
+++ sample-core/src/main/java/com/helpscout/beacon/sample/core/CoreActivity.kt
@@ -0,0 +1,12 @@
+package com.helpscout.beacon.sample.core
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CoreActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_core)
+    }
+}
