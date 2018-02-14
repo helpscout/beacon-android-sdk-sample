cffe7c047c4e7274fc43419463b86819df9e9fea
David González <malmstein@gmail.com>
2018-02-14 11:45:36 +0100

    updating Beacon id

--- sample-customisation/src/main/java/net/helpscout/sample/beacon/CustomisationApplication.java
+++ sample-customisation/src/main/java/net/helpscout/sample/beacon/CustomisationApplication.java
@@ -6,7 +6,7 @@ import com.helpscout.beacon.Beacon;
 
 public class CustomisationApplication extends Application {
 
-    private static String secureBeaconId = "0a63de347-8a4a-4fe7-9d73-6ae41421d140";
+    private static String secureBeaconId = "a63de347-8a4a-4fe7-9d73-6ae41421d140";
 
     @Override
     public void onCreate() {
