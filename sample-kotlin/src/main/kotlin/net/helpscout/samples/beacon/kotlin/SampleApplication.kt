c506d53c219f90ec415b392667f7d2609e5785d2
afflittoHS <144694780+afflittoHS@users.noreply.github.com>
2023-10-27 11:07:28 -0500

    remove testing beaconid

--- sample-kotlin/src/main/kotlin/net/helpscout/samples/beacon/kotlin/SampleApplication.kt
+++ sample-kotlin/src/main/kotlin/net/helpscout/samples/beacon/kotlin/SampleApplication.kt
@@ -6,7 +6,7 @@ import com.helpscout.beacon.Beacon
 class SampleApplication : Application() {
 
     // TODO replace with your Beacon Id from Help Scout
-    private val beaconId = "55fa783f-a0bb-402e-b2b0-dbd4b3d03146"
+    private val beaconId = ""
 
     override fun onCreate() {
         super.onCreate()
